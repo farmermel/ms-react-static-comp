07ef601c0794b0f48b2f8d8a6db4f5491ed35f90
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-12-31 14:05:03 -0700

    Make responsive and change images to small theme

--- src/App.js
+++ src/App.js
@@ -7,48 +7,49 @@ import './App.css';
 
 const stories = [
   {
-    img: './story1.jpg',
-    title: 'Winning at Fonts',
-    desc: 'or, Why you should care about typography',
-    authImg: './louisa.png',
-    authName: 'Louisa',
-    estTime: '2 min'
+    img: '../public/bedbugs.png',
+    title: 'Bedbugs',
+    desc: 'For New Yorkers an almost inevitable disaster. For the rest of us, avoid them with these tips.',
+    authImg: '../public/pamela.png',
+    authName: 'Pamela',
+    estTime: '11 min'
   },
   {
-    img: './story2.jpg',
-    title: 'Hoodlums: a dissertation',
-    desc: 'Examining villains in modern storytelling',
-    authImg: './alternate.png',
+    img: '../public/ant.png',
+    title: 'The Lives of Ants',
+    desc: 'These lil\' arthropods do more than you think!',
+    authImg: '../public/alternate.png',
     authName: 'Nathaniel',
     estTime: '10 min'
   },
   {
-    img: './story3.jpg',
-    title: 'Halloween is a year-round sport',
-    desc: 'Those who say otherwise just aren\'t trying hard enough.',
-    authImg: './leta.png',
+    img: '../public/capsuleperson.png',
+    title: 'My Dream House',
+    desc: 'Ten square feet IS livable; here\'s how.',
+    authImg: '../public/leta.png',
     authName: 'Leta',
     estTime: '8 min'
   },
-  {
-    img: './story4.jpg',
-    title: 'How to Improve Learning and Classroom Engagement with Humor',
-    desc: 'Okay, so first of all, everybody knows fart jokes are hilarious ...',
-    authImg: './pamela.png',
-    authName: 'Pamela',
-    estTime: '11 min'
+    {
+    img: '../public/story1.jpg',
+    title: 'Melena is the greatest',
+    desc: 'The Acacia',
+    authImg: '../public/louisa.png',
+    authName: 'Louisa',
+    estTime: '2 min'
   }
 ]
 
 const App = () => {
   return (
     <div className='App'>
-      {/* add a Header component (already created) */}
-      {/* add a Banner component (already created) */}
-      <Stories  storiesTitle="Today's Top Stories"
+      <Header />
+      <Banner />
+      <Stories  storiesTitle="Today's Smallest Stories"
                 stories={stories} />
-      {/* add another Stories component with storiesTitle 'Pop Culture', and just pass down the same stories array as the above Stories component (already created but will need some editing) */}
-      {/* add a Footer Component (you will need to create this component in the provided Footer.js file) */}
+      <Stories storiesTitle="Very Niche Culture"
+                stories={stories} />         
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  // WHAT GOES HERE?
+  return (
+    <div className='Footer'>
+      <a>&#169; 2017</a>
+    </div>
+    )
 }
 
 export default Footer;
